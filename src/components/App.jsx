1e565d35dfa8cf8d66c8557dde61041ca671c3d0
Piotr Poterek <peterpoterek0@gmail.com>
2024-03-04 02:23:43 +0100

    navbar styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Navigation />
 
       <Routes>
-        <Route path="/" element={<Phonebook />} />
+        <Route path="/contacts" element={<Phonebook />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { StyledNav, StyledUl, StyledLi, StyledLink } from './NavigationStyles';
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <StyledNav>
+      <StyledUl>
+        <StyledLi>
+          <StyledLink to="/register">📝 Register</StyledLink>
+        </StyledLi>
+        <StyledLi>
+          <StyledLink to="/login">🔑 Login</StyledLink>
+        </StyledLi>
+        <StyledLi>
+          <StyledLink to="/contacts">📞 Contacts</StyledLink>
+        </StyledLi>
+      </StyledUl>
+    </StyledNav>
+  );
 };
 
 export default Navigation;
