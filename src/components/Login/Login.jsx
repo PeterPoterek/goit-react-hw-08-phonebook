2a28161a02a7ce5280e8291731721e52251b25ae
Piotr Poterek <peterpoterek0@gmail.com>
2024-03-04 02:59:48 +0100

    Login Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,35 @@
+import React, { useState } from 'react';
+import {
+  StyledLoginContainer,
+  StyledLoginForm,
+  StyledLoginTitle,
+  StyledLoginLabel,
+  StyledLoginInput,
+  StyledLoginButton,
+} from './LoginStyles';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <StyledLoginContainer>
+      <StyledLoginForm>
+        <StyledLoginTitle>Login</StyledLoginTitle>
+        <form>
+          <StyledLoginLabel htmlFor="email">Email:</StyledLoginLabel>
+          <StyledLoginInput type="email" id="email" name="email" required />
+
+          <StyledLoginLabel htmlFor="password">Password:</StyledLoginLabel>
+          <StyledLoginInput
+            type="password"
+            id="password"
+            name="password"
+            required
+          />
+
+          <StyledLoginButton type="submit">Login</StyledLoginButton>
+        </form>
+      </StyledLoginForm>
+    </StyledLoginContainer>
+  );
 };
 
 export default Login;
